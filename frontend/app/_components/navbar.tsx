e9e6eda6ecc950cd315a922c2ba62d934289b677
rgpq25 <renzopq@hotmail.com>
2024-03-23 13:33:37 -0500

    Adding customizable mainLogo component for general use

--- frontend/app/_components/navbar.tsx
+++ frontend/app/_components/navbar.tsx
@@ -1,14 +1,12 @@
 import { Button } from "@/components/ui/button";
+import MainLogo from "@/public/main-logo";
 
 function Navbar() {
     return (
-        <nav className="fixed top-0 left-0 right-0 shadow-sm h-[60px] flex items-center px-2">
+        <nav className="fixed top-0 left-0 right-0 shadow-md h-[60px] bg-white flex items-center px-2">
             <div className="flex-1 flex flex-row justify-start items-center gap-2">
-                <img
-                    src="/mainLogo-light.png"
-                    className="w-12 h-12 object-contain"
-                />
-                <p className="font-medium">Red<span className="text-mainRed">Ex</span></p>
+                <MainLogo className="w-12 h-12"/>
+                <p className="font-semibold text-lg">Red<span className="text-mainRed">Ex</span></p>
             </div>
             <ul className="flex justify-center items-center gap-3 flex-1 ">
                 <Button variant="ghost">Nosotros</Button>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,37 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Navbar from "./_components/navbar";
+import MainLogo from "@/public/main-logo";
 
 export default function Home() {
     return (
-        <main className="flex flex-row h-screen justify-center items-center gap-18">
-			<Navbar/>
-            <section className="flex flex-col text-left">
-                <p className="text-8xl font-semibold">
-                    Red<span className="text-mainRed">Ex</span>
-                </p>
-                <p className="text-xl font-light">
-                    Mas alla de las fronteras, mas cerca de ti
-                </p>
-                <div>
-                    <Button className="mt-3 text-xl py-6" size="lg">
-                        Empieza ya{" "}
-                    </Button>
-                </div>
-            </section>
-            <img src="/mainLogo-light.png" className="h-[400px] object-cover" />
+        <main className="overflow-x-hidden">
+            <Navbar />
+            <div className="flex flex-row w-dvw h-dvh justify-center items-center gap-18">
+                <section className="flex flex-col text-left">
+                    <p className="text-8xl font-semibold">
+                        Red<span className="text-mainRed">Ex</span>
+                    </p>
+                    <p className="text-xl font-light">
+                        Mas alla de las fronteras, mas cerca de ti
+                    </p>
+                    <div>
+                        <Button className="mt-3 text-xl py-6" size="lg">
+                            Empieza ya{" "}
+                        </Button>
+                    </div>
+                </section>
+                <MainLogo className="h-[310px] w-[310px]"/>
+            </div>
+            <div className="flex flex-row w-screen h-screen justify-center items-center gap-18">
+                <p>nosotros</p>
+            </div>
+            <div className="flex flex-row w-screen h-screen justify-center items-center gap-18">
+                <p>servicios</p>
+            </div>
+            <div className="flex flex-row w-screen h-screen justify-center items-center gap-18">
+                <p>preguntas</p>
+            </div>
         </main>
     );
 }
