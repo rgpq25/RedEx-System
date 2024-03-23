87c41c873ef320475b4f958181ae9feca52a7369
rgpq25 <renzopq@hotmail.com>
2024-03-23 11:08:19 -0500

    Starting welcome page

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        "mainRed": "rgb(var(--mainRed) / <alpha-value>)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
