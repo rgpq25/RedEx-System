8a2b263b5962ade8de76ab6c92f8a20f29c21038
Diego Veramendi <v.dvm.dvm@gmail.com>
2024-04-30 12:04:20 -0500

    fix(psov066): Change rutas generation with dataset 1000_j1.csv

--- pso/psov066/main.java
+++ pso/psov066/main.java
@@ -106,104 +106,104 @@ public class main {
             System.out.println("Tiempo de ejecución (grafoVuelos): " + (float) (duration0 / 1000000000) + " segundos");
             //grafoVuelos.imprimirVuelos();
 
-            //long startTime = System.nanoTime();
+            long startTime = System.nanoTime();
             //HashMap<String, ArrayList<PlanRuta>> rutas = grafoVuelos.buscarTodasLasRutas();
-            /*HashMap<String, ArrayList<PlanRuta>> rutas = new HashMap<String, ArrayList<PlanRuta>>();
+            HashMap<String, ArrayList<PlanRuta>> rutas = new HashMap<String, ArrayList<PlanRuta>>();
             try {
                 rutas = grafoVuelos.buscarTodasLasRutas();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (ExecutionException e) {
                 e.printStackTrace();
-            }*/
+            }
 
-            /*System.out.println("Checking if ORIGIN-DESTINATION has atleast 1 route available");
+            System.out.println("Checking if ORIGIN-DESTINATION has atleast 1 route available");
             for (Map.Entry<String, ArrayList<PlanRuta>> entry : rutas.entrySet()) {
                 String key = entry.getKey();
                 ArrayList<PlanRuta> value = entry.getValue();
                 if (value.size() == 0) {
                     System.out.println("No route available for " + key);
                 }
-            }*/
+            }
 
-            /*long endTime = System.nanoTime();
+            long endTime = System.nanoTime();
             long duration = endTime - startTime;
 
-            System.out.println("Tiempo de ejecución de la ordenación (buscarTodasLasRutas): " + (float) (duration / 1000000000) + " segundos");*/
+            System.out.println("Tiempo de ejecución de la ordenación (buscarTodasLasRutas): " + (float) (duration / 1000000000) + " segundos");
 
 
-            //funciones.saveRutesTxt(rutas, rutasPath, 1048576);
+            funciones.saveRutesTxt(rutas, rutasPath, 1048576);
             
-            HashMap<String, ArrayList<PlanRuta>> rutas2 = RutasFileReader.readRutasFiles(rutasPath,vuelos_map);
-            //iterate hashmap
-            for (Map.Entry<String, ArrayList<PlanRuta>> entry : rutas2.entrySet()) {
-                String key = entry.getKey();
-                ArrayList<PlanRuta> value = entry.getValue();
+            // HashMap<String, ArrayList<PlanRuta>> rutas2 = RutasFileReader.readRutasFiles(rutasPath,vuelos_map);
+            // //iterate hashmap
+            // for (Map.Entry<String, ArrayList<PlanRuta>> entry : rutas2.entrySet()) {
+            //     String key = entry.getKey();
+            //     ArrayList<PlanRuta> value = entry.getValue();
                 
-                for (PlanRuta planRuta : value) {
-                    //check if planRuta is null
-                    if (planRuta==null || planRuta.getVuelos() == null) {
-                        System.out.println("Key: " + key);
-                        System.out.println("PlanRuta is null");
-                    }
-                }
-            }
+            //     for (PlanRuta planRuta : value) {
+            //         //check if planRuta is null
+            //         if (planRuta==null || planRuta.getVuelos() == null) {
+            //             System.out.println("Key: " + key);
+            //             System.out.println("PlanRuta is null");
+            //         }
+            //     }
+            // }
             
-            //funciones.imprimirTodasLasRutas(rutas2); 
+            // //funciones.imprimirTodasLasRutas(rutas2); 
             
             
 
-            //tengo ArrayList<Paquete> paquetes, tengo HashMap<String, ArrayList<PlanRuta>> rutas
-            //quiero HashMap<Integer, ArrayList<PlanRuta>>
-            long startTime3 = System.nanoTime();
-            HashMap<Integer, ArrayList<PlanRuta>> rutas_validas = funciones.getRutasValidasPorPaquete(paquetes, rutas2);
-            long endTime3 = System.nanoTime();
-            long duration3 = endTime3 - startTime3;
-            System.out.println("Tiempo de (getRutasValidasPorPaquete): " + (float) (duration3 / 1000000000) + " segundos");
+            // //tengo ArrayList<Paquete> paquetes, tengo HashMap<String, ArrayList<PlanRuta>> rutas
+            // //quiero HashMap<Integer, ArrayList<PlanRuta>>
+            // long startTime3 = System.nanoTime();
+            // HashMap<Integer, ArrayList<PlanRuta>> rutas_validas = funciones.getRutasValidasPorPaquete(paquetes, rutas2);
+            // long endTime3 = System.nanoTime();
+            // long duration3 = endTime3 - startTime3;
+            // System.out.println("Tiempo de (getRutasValidasPorPaquete): " + (float) (duration3 / 1000000000) + " segundos");
 
-            //PARTE DE PSO
-            //PSO pso = new PSO();
-            long startTime2 = System.nanoTime();
-            int[] bestComb = PSO.pso(paquetes, rutas_validas,aeropuertos,vuelos_map, 100, 1000, 0.7, 1.4, 1.4);
-            long endTime2 = System.nanoTime();
-            long duration2 = endTime2 - startTime2;
-            System.out.println("Tiempo de ejecución del algoritmo PSO: " + (float) (duration2 / 1000000000) + " segundos");
+            // //PARTE DE PSO
+            // //PSO pso = new PSO();
+            // long startTime2 = System.nanoTime();
+            // int[] bestComb = PSO.pso(paquetes, rutas_validas,aeropuertos,vuelos_map, 100, 1000, 0.7, 1.4, 1.4);
+            // long endTime2 = System.nanoTime();
+            // long duration2 = endTime2 - startTime2;
+            // System.out.println("Tiempo de ejecución del algoritmo PSO: " + (float) (duration2 / 1000000000) + " segundos");
 
-            List<Integer> bestCombList = new ArrayList<>();
-            for (int i = 0; i < bestComb.length; i++) {
-                bestCombList.add(bestComb[i]);
-            }
-            //double bestFit = PSO.fitness(Arrays.asList(ArrayUtils.toObject(bestComb)), paquetes, rutas);
-            double bestFit = PSO.fitness(bestCombList, paquetes, rutas_validas, aeropuertos,vuelos_map);
-            //print bestFit
-            System.out.println("Best fitness: " + bestFit);
-            //IMPRIME LA MEJOR SOLUCION
-            for (int i = 0; i < bestComb.length; i++) {
-                ArrayList<PlanRuta> planRutas = rutas_validas.get(paquetes.get(i).getId());
-                //print paquete
-                paquetes.get(i).print();
-                if(planRutas == null){
-                    System.out.println("No hay rutas para el paquete " + i);
-                }
-                else{
-                    System.out.println("La mejor ruta para el paquete " + i + " fue:");
-                    System.out.println(planRutas.get(bestComb[i]));
-                    //verifica la hora salida del primer vuelo
-                    long tiempoRecepcion = paquetes.get(i).getFecha_recepcion().getTime();
-                    long tiempoPartidaRuta = planRutas.get(bestComb[i]).getVuelos().get(0).getFecha_salida().getTime();
-                    long tiempoLlegadaRuta = planRutas.get(bestComb[i]).getVuelos().get(planRutas.get(bestComb[i]).getVuelos().size() - 1).getFecha_llegada().getTime();
-                    if(tiempoRecepcion > tiempoPartidaRuta){
-                        System.out.println("El paquete " + i + " se recibió después de la salida del primer vuelo");
-                    }
-                    if(tiempoLlegadaRuta > paquetes.get(i).getFecha_maxima_entrega().getTime()){
-                        System.out.println("El paquete " + i + " llega después de la fecha máxima de entrega");
-                    }
-                }
-                System.out.println();
-            }
-            System.setOut(originalStdOut);
-            bufferedWriterOutput.close();
-            System.out.println("Fin");
+            // List<Integer> bestCombList = new ArrayList<>();
+            // for (int i = 0; i < bestComb.length; i++) {
+            //     bestCombList.add(bestComb[i]);
+            // }
+            // //double bestFit = PSO.fitness(Arrays.asList(ArrayUtils.toObject(bestComb)), paquetes, rutas);
+            // double bestFit = PSO.fitness(bestCombList, paquetes, rutas_validas, aeropuertos,vuelos_map);
+            // //print bestFit
+            // System.out.println("Best fitness: " + bestFit);
+            // //IMPRIME LA MEJOR SOLUCION
+            // for (int i = 0; i < bestComb.length; i++) {
+            //     ArrayList<PlanRuta> planRutas = rutas_validas.get(paquetes.get(i).getId());
+            //     //print paquete
+            //     paquetes.get(i).print();
+            //     if(planRutas == null){
+            //         System.out.println("No hay rutas para el paquete " + i);
+            //     }
+            //     else{
+            //         System.out.println("La mejor ruta para el paquete " + i + " fue:");
+            //         System.out.println(planRutas.get(bestComb[i]));
+            //         //verifica la hora salida del primer vuelo
+            //         long tiempoRecepcion = paquetes.get(i).getFecha_recepcion().getTime();
+            //         long tiempoPartidaRuta = planRutas.get(bestComb[i]).getVuelos().get(0).getFecha_salida().getTime();
+            //         long tiempoLlegadaRuta = planRutas.get(bestComb[i]).getVuelos().get(planRutas.get(bestComb[i]).getVuelos().size() - 1).getFecha_llegada().getTime();
+            //         if(tiempoRecepcion > tiempoPartidaRuta){
+            //             System.out.println("El paquete " + i + " se recibió después de la salida del primer vuelo");
+            //         }
+            //         if(tiempoLlegadaRuta > paquetes.get(i).getFecha_maxima_entrega().getTime()){
+            //             System.out.println("El paquete " + i + " llega después de la fecha máxima de entrega");
+            //         }
+            //     }
+            //     System.out.println();
+            // }
+            // System.setOut(originalStdOut);
+            // bufferedWriterOutput.close();
+            // System.out.println("Fin");
         }catch (IOException e) {
             // Handle IO Exception
             e.printStackTrace();
