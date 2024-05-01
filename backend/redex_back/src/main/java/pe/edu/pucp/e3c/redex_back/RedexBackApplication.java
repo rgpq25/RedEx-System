5ac418b85942b2de4d209f452227c21ec238c050
Jose <a20190202@pucp.edu.pe>
2024-04-30 23:30:27 -0500

    backend, prueba con h2 inmemory database

---
+++ backend/redex_back/src/main/java/pe/edu/pucp/e3c/redex_back/RedexBackApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.e3c.redex_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedexBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedexBackApplication.class, args);
+	}
+
+}
---
+++ backend/redex_back/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ubicacion(
+    id varchar(10) NOT NULL PRIMARY KEY,
+    pais varchar(50) NOT NULL,
+    ciudad varchar(50) NOT NULL,
+    ciudad_abreviada varchar(10) NOT NULL,
+    zona_horaria varchar(20) NOT NULL
+);
