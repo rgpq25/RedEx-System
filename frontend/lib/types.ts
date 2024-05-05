fe0c79a81eaa133ad7a0534b02bdeb6c0643547e
Diego Veramendi <v.dvm.dvm@gmail.com>
2024-05-05 12:04:39 -0500

    feat(types): Add DB types

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -25,5 +25,82 @@ export type RowPackageRouteType = {
     isCurrent: boolean;
 }
 
-
 export type RoleType = "admin" | "operator" | "user" | null;
+
+export enum Operacion {
+    Envios = "Envios",
+    Aeropuertos = "Aeropuertos",
+    Vuelos = "Vuelos"
+}
+
+export type Coordenadas = {
+    latitud: number;
+    longitud: number;
+};
+
+export type Ubicacion = {
+    id: number;
+    coordenadas: {
+        latitud: number;
+        longitud: number;
+    };
+    continente: string;
+    pais: string;
+    ciudad: string;
+    ciudad_abreviada: string;
+    zona_horaria: string;
+}
+
+export type Aeropuerto = {
+    id: number;
+    ubicacion: Ubicacion;
+    capacidad_maxima: number;
+}
+
+export type PlanVuelo = {
+    id: number;
+    ubicacion_origen: Ubicacion;
+    ubicacion_destino: Ubicacion;
+    hora_ciudad_origen: string;
+    hora_ciudad_destino: string;
+    capacidad_maxima: number;
+}
+
+export type Vuelo = {
+    id: number;
+    plan_vuelo: PlanVuelo;
+    fecha_origen: string;
+    fecha_destino: string;
+    coordenadas_actual : Coordenadas;
+    tiempo_estimado: number;
+    capacidad_utilizada: number;
+    estado: string;
+}
+
+export type PlanRuta = {
+    id: number;
+    ubicacion_origen: Ubicacion;
+    ubicacion_destino: Ubicacion;
+    fecha_inicio: string;
+    fecha_fin: string;
+    vuelos: Vuelo[];
+}
+
+export type Paquete = {
+    id: number;
+    aeropuerto_actual: Aeropuerto;
+    entregado: boolean;
+    envio: Envio;
+    fecha_entrega: string;
+}
+
+export type Envio = {
+    id: number;
+    ubicacion_origen: Ubicacion;
+    ubicacion_destino: Ubicacion;
+    fecha_recepcion: string;
+    fecha_limite_entrega: string;
+    estado: string;
+    cantidad_paquetes: number;
+    codigo_seguridad: string;
+}
