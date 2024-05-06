7f789d178fcd3fe7e9460b6d8d2d6b5743c12439
Diego Veramendi <v.dvm.dvm@gmail.com>
2024-05-05 22:07:36 -0500

    fix(sidebar): Add sidebar to testing

--- frontend/app/(main)/simulation/page.tsx
+++ frontend/app/(main)/simulation/page.tsx
@@ -9,6 +9,7 @@ import { PackageTable } from "./_components/package-table";
 import Visualizator from "./_components/visualizator";
 import InfoNotation1 from "./_components/info-notation1";
 import Sidebar from "@/app/_components/sidebar";
+import Map from "@/components/map/map";
 
 type TabType = "weekly" | "colapse";
 
@@ -20,7 +21,10 @@ function SimulationPage() {
     return (
         <main className="px-10 py-5">
             <h1>Visualizador de simulación</h1>
-            <Visualizator className="mt-2" />
+            <section className="relative">
+                <Map />
+                <Sidebar envios={envios} vuelos={vuelos} aeropuertos={aeropuertos} className="absolute index-2 top-4 left-4 bottom-4" />
+            </section>
             <section className="flex flex-col 2xl:flex-row 2xl:gap-10 gap-4  w-full mt-3">
                 <div className="flex flex-col 2xl:w-[600px]">
                     <div className="flex items-center ">
@@ -60,7 +64,6 @@ function SimulationPage() {
 
                 <PackageTable />
             </section>
-            <Sidebar envios={envios} vuelos={vuelos} aeropuertos={aeropuertos} />
         </main>
     );
 }
