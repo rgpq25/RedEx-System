7d28a86c9c130c96b2e47730397a708afc3d23b9
Jose <a20190202@pucp.edu.pe>
2024-06-03 16:57:56 -0500

    primeros cmabios

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    env: {
+      API_URL_BASE: "https://inf226-981-3c.inf.pucp.edu.pe"
+    },
+    images: { unoptimized: true },
+    output: "export",
+    trailingSlash: true,
+    swcMinify: false,
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+  };
+  
 export default nextConfig;
+  
