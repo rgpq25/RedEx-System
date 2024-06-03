0d528c53680cb9f0cd70d100a686ca57be1352f8
Santiago Castro García <scastrog@pucp.edu.pe>
2024-06-03 17:07:47 -0500

    Api registrar envios registrarAllByString

--- backend/redex_back/src/main/java/pucp/e3c/redex_back/model/Funciones.java
+++ backend/redex_back/src/main/java/pucp/e3c/redex_back/model/Funciones.java
@@ -282,7 +282,7 @@ public class Funciones {
         return String.join("-", partes);
     }
 
-    public static Envio stringToEnvio(String line, HashMap<String, Ubicacion> ubicacionMap, int idSimulacion,
+    public static Envio stringToEnvio(String line, HashMap<String, Ubicacion> ubicacionMap, Simulacion simulacion,
             AeropuertoRepository aeropuertoRepository) {
         String[] parts = line.split("-");
         Envio envio = new Envio();
@@ -308,9 +308,10 @@ public class Funciones {
 
         envio.fillData(origen, destino, fecha_recepcion_GMT0, fecha_maxima_entrega_GMT0);
         envio.setCantidadPaquetes(cantidadPaquetes);
-        envio.setCodigoSeguridad("123456");
-        Simulacion simulacion = new Simulacion();
-        simulacion.setId(idSimulacion);
+        Random random = new Random();
+        int randomNumber = random.nextInt(900000) + 100000;
+        envio.setCodigoSeguridad(Integer.toString(randomNumber));
+
         envio.setSimulacionActual(simulacion);
 
         return envio;
