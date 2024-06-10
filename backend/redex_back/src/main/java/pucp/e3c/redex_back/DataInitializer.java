f8ffac306d6340d5b32608f41ee0f3e6ca06526b
Jose <a20190202@pucp.edu.pe>
2024-06-10 17:13:37 -0500

    false init

--- backend/redex_back/src/main/java/pucp/e3c/redex_back/DataInitializer.java
+++ backend/redex_back/src/main/java/pucp/e3c/redex_back/DataInitializer.java
@@ -234,7 +234,7 @@ public class DataInitializer {
          * paqueteService.register(paquete);
          * }
          */
-        boolean inicializar_paquetes_operaciones_dia_dia = true;
+        boolean inicializar_paquetes_operaciones_dia_dia = false;
         if (inicializar_paquetes_operaciones_dia_dia) {
             //inicializaPaquetesDiaDia(aeropuertos, ubicacionMap, planVuelos);
             incializacionFijaPaquetesDiaDia(aeropuertos, ubicacionMap, planVuelos, 100); //100,250,500
