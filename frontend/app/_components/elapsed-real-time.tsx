7dca91c6bc5519171423a993ab8e17dbfddc6dba
Diego Veramendi <v.dvm.dvm@gmail.com>
2024-06-11 16:54:14 -0500

    fix(visualizator): Change language to times labels

--- frontend/app/_components/elapsed-real-time.tsx
+++ frontend/app/_components/elapsed-real-time.tsx
@@ -1,7 +1,7 @@
 function ElapsedRealTime({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="border rounded-3xl border-red-700 text-red-700 bg-red-100/70 py-1 proportional-nums w-[242px] text-start shadow-md px-3 flex flex-row gap-1 items-center">
-			<a className="font-medium">Real elapsed time:</a> <p>{children}</p>
+		<div className="border rounded-3xl border-red-700 text-red-700 bg-red-100/70 py-1 proportional-nums w-fit text-start shadow-md px-3 flex flex-row gap-1 items-center justify-end">
+			<a className="font-medium">Tiempo real transcurrido:</a> <p>{children}</p>
 		</div>
 	);
 }
--- frontend/app/_components/elapsed-simu-time.tsx
+++ frontend/app/_components/elapsed-simu-time.tsx
@@ -1,7 +1,7 @@
 function ElapsedSimuTime({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="border rounded-3xl border-yellow-700 text-yellow-700 bg-yellow-100/70 py-1 proportional-nums w-[287px] text-start shadow-md px-3 flex flex-row gap-1 items-center">
-			<a className="font-medium">Simulation elapsed time:</a> <p>{children}</p>
+		<div className="border rounded-3xl border-yellow-700 text-yellow-700 bg-yellow-100/70 py-1 proportional-nums w-fit text-start shadow-md px-3 flex flex-row gap-1 items-center justify-end">
+			<a className="font-medium">Tiempo simulado transcurrido:</a> <p>{children}</p>
 		</div>
 	);
 }
