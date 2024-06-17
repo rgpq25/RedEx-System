3382477a43bd487a3ad437c75e51e9d62c42ff0d
Santiago Castro García <scastrog@pucp.edu.pe>
2024-06-16 21:45:11 -0500

    Update Envio.java

--- backend/redex_back/src/main/java/pucp/e3c/redex_back/model/Envio.java
+++ backend/redex_back/src/main/java/pucp/e3c/redex_back/model/Envio.java
@@ -34,7 +34,7 @@ public class Envio {
     private Date fechaRecepcion;
 
     @Temporal(TemporalType.TIMESTAMP)
-    private Date fechaLimiteEntrega; //EN UTC
+    private Date fechaLimiteEntrega; // EN UTC
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date fechaLimiteEntregaZonaHorariaDestino;
@@ -73,7 +73,6 @@ public class Envio {
     public void fillData(Ubicacion origen, Ubicacion destino, Date fecha_recepcion_GMT0) {
         this.setUbicacionOrigen(origen);
         this.setUbicacionDestino(destino);
-        this.setFechaRecepcion(fecha_recepcion_GMT0);
 
         this.setEstado("En proceso");
 
@@ -83,49 +82,17 @@ public class Envio {
         } else {
             agregar = 2;
         }
+        this.setFechaRecepcion(fecha_recepcion_GMT0);
 
-        Date fecha_recepcion_origen = Funciones.convertTimeZone(
-            fecha_recepcion_GMT0,"UTC",origen.getZonaHoraria());
-        Date fecha_maxima_entrega_GMTDestino = Funciones.addDays(fecha_recepcion_origen, agregar);
+        Date fecha_maxima_entrega_GMTDestino = Funciones.addDays(fechaRecepcion, agregar);
         Date fecha_maxima_entrega_GMT0 = Funciones.convertTimeZone(
-            fecha_maxima_entrega_GMTDestino,destino.getZonaHoraria(),"UTC");     
-
-        /*SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");  
-        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss"); 
-        Date fecha_recepcion_GMTOrigin = Funciones.parseDateString(dateFormat.format(fecha_recepcion_origen) + " " + timeFormat.format(fecha_recepcion_origen));
-        System.out.println("fecha_recepcion_GMTOrigin: "+fecha_recepcion_GMTOrigin);
-        Date fecha_proceso_1_GMT0 = Funciones.convertTimeZone(fecha_recepcion_GMTOrigin, destino.getZonaHoraria(), "UTC");
-        System.out.println("fecha_proceso_1_GMT0: "+fecha_proceso_1_GMT0);
-        Date fecha_proceso_1_destino = Funciones.convertTimeZone(fecha_proceso_1_GMT0, "UTC", destino.getZonaHoraria());
-        System.out.println("fecha_proceso_1_destino: "+fecha_proceso_1_destino);
-        //Ahora trabajo con fecha_recepcion_GMT0 y fecha_proceso_1_GMT0
-        if(fecha_proceso_1_GMT0.before(fecha_recepcion_GMT0) || fecha_proceso_1_GMT0.equals(fecha_recepcion_GMT0)){
-            //agrega un dia entero a fecha_proceso_1_GMT0
-            fecha_proceso_1_GMT0 = Funciones.addDays(fecha_proceso_1_GMT0, 1);
-        }
-        if(agregar==2){
-            //agrega un dia entero a fecha_proceso_1_GMT0
-            fecha_proceso_1_GMT0 = Funciones.addDays(fecha_proceso_1_GMT0, 1);
-        }*/
-        /*Date fecha_fin_proceso_destino = Funciones.convertTimeZone(fecha_proceso_1_GMT0, "UTC", destino.getZonaHoraria());
-        System.out.println("=====================================");
-        System.out.println("Fecha recepcion en hora ciudad origen: " + fecha_recepcion_origen);
-        System.out.println("Fecha proceso entrega: " + fecha_proceso_1_GMT0);
-        System.out.println(destino.getZonaHoraria());
-        System.out.println("Fecha proceso entrega en hora ciudad destino: " + fecha_fin_proceso_destino);
-        System.out.println("=====================================");*/
-
+                fecha_maxima_entrega_GMTDestino,
+                destino.getZonaHoraria(),
+                "UTC");
         this.setFechaLimiteEntrega(fecha_maxima_entrega_GMT0);
-        //this.setFechaLimiteEntrega(fecha_proceso_1_GMT0);
-        //System.out.println("ASIGNANDO Fecha limite entrega GMT0: " + fecha_maxima_entrega_GMT0);
-        this.setFechaLimiteEntregaZonaHorariaDestino(fecha_maxima_entrega_GMTDestino);
-        //this.setFechaLimiteEntregaZonaHorariaDestino(fecha_fin_proceso_destino); 
-        //System.out.println("ASIGNANDO Fecha limite entrega GMT DESTINO: " + fecha_maxima_entrega_GMTDestino);       
         this.setCantidadPaquetes(1);
         this.setCodigoSeguridad("123456");
     }
-
-    
 
     public Cliente getEmisor() {
         return emisor;
@@ -224,13 +191,9 @@ public class Envio {
         this.simulacionActual = simulacionActual;
     }
 
-
-
     public Date getFechaLimiteEntregaZonaHorariaDestino() {
         return fechaLimiteEntregaZonaHorariaDestino;
     }
-
-
 
     public void setFechaLimiteEntregaZonaHorariaDestino(Date fechaLimiteEntregaZonaHorariaDestino) {
         this.fechaLimiteEntregaZonaHorariaDestino = fechaLimiteEntregaZonaHorariaDestino;
