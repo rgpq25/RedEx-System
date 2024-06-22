603e15951f140269f2a288d2548bd354d724c342
Jose <a20190202@pucp.edu.pe>
2024-06-21 23:03:27 -0500

    cambio tiempo op dia a dia

--- backend/redex_back/src/main/java/pucp/e3c/redex_back/DataInitializer.java
+++ backend/redex_back/src/main/java/pucp/e3c/redex_back/DataInitializer.java
@@ -239,7 +239,7 @@ public class DataInitializer {
         CompletableFuture.runAsync(() -> {
             algoritmo.loopPrincipalDiaADia(aeropuertosLoop, planVuelosLoop,
                     vueloService, planRutaService, paqueteService, planRutaXVueloService, aeropuertoService,
-                    240);
+                    120);
         });
 
     }
