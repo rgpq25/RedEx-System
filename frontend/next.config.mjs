b57fef8bc8b0b2fc43bc097444ce5f89a92ddbb6
Jose <a20190202@pucp.edu.pe>
2024-06-29 16:52:59 -0500

    changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // NEXT_PUBLIC_API: "https://inf226-981-3c.inf.pucp.edu.pe",
-        // NEXT_PUBLIC_SOCKET: "wss://inf226-981-3c.inf.pucp.edu.pe:8080",
-        NEXT_PUBLIC_API: "http://localhost:8080",
-        NEXT_PUBLIC_SOCKET: "ws://localhost:8080",
+        NEXT_PUBLIC_API: "https://inf226-981-3c.inf.pucp.edu.pe",
+        NEXT_PUBLIC_SOCKET: "wss://inf226-981-3c.inf.pucp.edu.pe",
+        //NEXT_PUBLIC_API: "http://localhost:8080",
+        //NEXT_PUBLIC_SOCKET: "ws://localhost:8080",
     },
     images: { unoptimized: true },
     output: "export",
