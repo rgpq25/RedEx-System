71457ec78f8913e007dcc4408e9fb66e5256e5ec
Diego Veramendi <v.dvm.dvm@gmail.com>
2024-06-30 12:51:21 -0500

    feat(sidebar-shipment): Add stepper for route package

--- frontend/app/_components/sidebar-shipment.tsx
+++ frontend/app/_components/sidebar-shipment.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-
-import { useState } from "react";
-
+import { useState, useEffect, useMemo } from "react";
 import { ChevronsRight, ChevronsLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
@@ -12,7 +10,19 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { H2, H3, Large, Lead, Muted, Small } from "@/components/ui/typography";
-
+import {
+    Step,
+    StepDescription,
+    StepIcon,
+    StepIndicator,
+    StepNumber,
+    StepSeparator,
+    StepStatus,
+    StepTitle,
+    Stepper,
+    useSteps,
+} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import { Envio, Paquete, Vuelo } from "@/lib/types";
 import { formatDateTimeLongShort } from "@/lib/date";
 import Chip from "@/components/ui/chip";
@@ -20,7 +30,6 @@ import { ChipColors, PackageStatusVariant } from "@/lib/types";
 import { getPackageState } from "@/components/map/envio-modal";
 import { EstadoPaquete } from "@/lib/types";
 
-// Datos de estado del paquete
 const badgePackageStatusData = [
     { name: "En almacén origen", color: "gray" as ChipColors },
     { name: "Volando", color: "blue" as ChipColors },
@@ -30,7 +39,6 @@ const badgePackageStatusData = [
     { name: "Atrasado", color: "red" as ChipColors },
 ];
 
-// Componente para mostrar el chip de estado
 const StatusChip = ({ status }: { status: EstadoPaquete }) => (
     <Chip
         color={status.color}
@@ -40,7 +48,6 @@ const StatusChip = ({ status }: { status: EstadoPaquete }) => (
     </Chip>
 );
 
-// Función para calcular el tiempo restante
 const calculateRemainingTime = (fechaLlegadaUltimoVuelo: Date, currentTime: Date) => {
     const diffTime = fechaLlegadaUltimoVuelo.getTime() - currentTime.getTime();
     if (diffTime < 0) return "0 minutos";
@@ -49,11 +56,120 @@ const calculateRemainingTime = (fechaLlegadaUltimoVuelo: Date, currentTime: Date
     return diffHours === 0 ? `${diffMinutes} minutos` : `${diffHours} horas y ${diffMinutes} minutos`;
 };
 
-// Componente SidebarShipment
+const generateAeropuertosData = (selectedPackage: Paquete, shipment: Envio) => {
+    return selectedPackage?.planRutaActual?.vuelos?.map((vuelo: Vuelo, index: number) => {
+        const aeropuertoData = {
+            type: "aeropuerto",
+            id: `${vuelo.id}-aeropuerto`,
+            ciudad: vuelo.planVuelo.ciudadOrigen.ciudad,
+            fechaSalida: vuelo.fechaSalida,
+            tiempoEnAlmacen: (() => {
+                if (index === 0) {
+                    let tiempoRegistro = new Date(shipment?.fechaRecepcion);
+                    let tiempoSalida = new Date(vuelo?.fechaSalida);
+                    if (isNaN(tiempoRegistro.getTime()) || isNaN(tiempoSalida.getTime())) {
+                        return "Fecha no válida";
+                    }
+                    let diffTime = tiempoSalida.getTime() - tiempoRegistro.getTime();
+                    let diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+                    let diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+                    return diffHours === 0 ? `${diffMinutes} minutos` : `${diffHours} horas y ${diffMinutes} minutos`;
+                } else {
+                    if (selectedPackage?.planRutaActual?.vuelos[index - 1] === undefined) return "";
+                    let tiempoLlegada = new Date(selectedPackage?.planRutaActual?.vuelos[index - 1]?.fechaLlegada);
+                    let tiempoSalida = new Date(vuelo.fechaSalida);
+                    if (isNaN(tiempoLlegada.getTime()) || isNaN(tiempoSalida.getTime())) {
+                        return "Fecha no válida";
+                    }
+                    let diffTime = tiempoSalida.getTime() - tiempoLlegada.getTime();
+                    let diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+                    let diffMinutes = Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60));
+                    return diffHours === 0 ? `${diffMinutes} minutos` : `${diffHours} horas y ${diffMinutes} minutos`;
+                }
+            })(),
+        };
+        return aeropuertoData;
+    });
+};
+
+const generateVuelosData = (selectedPackage: Paquete) => {
+    return selectedPackage?.planRutaActual?.vuelos?.map((vuelo: Vuelo) => {
+        return {
+            type: "vuelo",
+            id: `${vuelo.id}-vuelo`,
+            origen: vuelo.planVuelo.ciudadOrigen.ciudad,
+            destino: vuelo.planVuelo.ciudadDestino.ciudad,
+            fechaSalida: vuelo.fechaSalida,
+            fechaLlegada: vuelo.fechaLlegada,
+        };
+    });
+};
+
 const SidebarShipment = ({ shipment, currentTime }: { shipment: Envio | undefined; currentTime: Date | undefined }) => {
     const [visible, setVisible] = useState<boolean>(true);
     const [selectedIdPackage, setSelectedIdPackage] = useState<number>();
     const selectedPackage = shipment?.paquetes?.find((pkg: Paquete) => pkg.id === selectedIdPackage);
+
+    const intercalateSteps = (aeropuertos: any, vuelos: any, selectedPackage: Paquete) => {
+        const steps = [];
+        for (let i = 0; i < vuelos?.length; i++) {
+            steps.push(aeropuertos[i]);
+            steps.push(vuelos[i]);
+        }
+        if (selectedPackage?.planRutaActual?.vuelos?.length === undefined || selectedPackage?.planRutaActual?.vuelos?.length === 0)
+            return steps;
+        if (selectedPackage?.planRutaActual?.vuelos?.length > 0) {
+            const ultimoVuelo = selectedPackage?.planRutaActual?.vuelos[selectedPackage?.planRutaActual?.vuelos?.length - 1];
+            steps.push({
+                type: "aeropuerto",
+                id: `${ultimoVuelo.id}-aeropuerto-destino`,
+                ciudad: ultimoVuelo.planVuelo.ciudadDestino.ciudad,
+                fechaSalida: ultimoVuelo.fechaLlegada,
+                tiempoEnAlmacen: "Destino final",
+            });
+        }
+        return steps;
+    };
+
+    const stepsData = useMemo(() => {
+        if (selectedPackage !== undefined && shipment !== undefined) {
+            const aeropuertos = generateAeropuertosData(selectedPackage, shipment);
+            const vuelos = generateVuelosData(selectedPackage);
+            return intercalateSteps(aeropuertos, vuelos, selectedPackage);
+        }
+        return [];
+    }, [selectedPackage, shipment]);
+
+    const { activeStep, setActiveStep } = useSteps({
+        index: 0,
+        count: stepsData.length,
+    });
+
+    useEffect(() => {
+        if (stepsData.length > 0 && currentTime) {
+            const activeIndex = calculateActiveStep(stepsData, currentTime);
+            setActiveStep(activeIndex);
+        }
+    }, [stepsData, currentTime, setActiveStep]);
+
+    const calculateActiveStep = (steps: any[], currentTime: Date | undefined) => {
+        if (!currentTime) return 0;
+        for (let i = 0; i < steps.length; i++) {
+            if (steps[i].type === "aeropuerto") {
+                const stepDate = new Date(steps[i].fechaSalida);
+                if (currentTime < stepDate) {
+                    return i;
+                }
+            } else if (steps[i].type === "vuelo") {
+                const fechaSalida = new Date(steps[i].fechaSalida);
+                const fechaLlegada = new Date(steps[i].fechaLlegada);
+                if (currentTime > fechaSalida && currentTime < fechaLlegada) {
+                    return i;
+                }
+            }
+        }
+        return steps.length;
+    };
 
     return (
         <>
@@ -180,7 +296,7 @@ const SidebarShipment = ({ shipment, currentTime }: { shipment: Envio | undefine
                                 selectedPackage?.planRutaActual === undefined ||
                                 selectedPackage?.planRutaActual?.vuelos === undefined ||
                                 selectedPackage?.planRutaActual?.vuelos.length === 0 ? (
-                                    <Muted>Rutas en planificacion.</Muted>
+                                    <Muted className='inline-flex'>Ruta en planificacion...</Muted>
                                 ) : (
                                     <div className='flex flex-row gap-4'>
                                         <div className='w-full'>
@@ -201,72 +317,56 @@ const SidebarShipment = ({ shipment, currentTime }: { shipment: Envio | undefine
                                                         const fechaLlegadaUltimoVuelo = new Date(ultimoVuelo.fechaLlegada);
                                                         const tiempoRestante = calculateRemainingTime(fechaLlegadaUltimoVuelo, currentTime);
 
-                                                        return <Small>{`Tiempo restante: ${tiempoRestante}`}</Small>;
+                                                        return <Small className='mb-4'>{`Tiempo restante: ${tiempoRestante}`}</Small>;
                                                     })()}
                                                 </>
                                             )}
-                                            {selectedPackage?.planRutaActual?.vuelos?.map((vuelo: Vuelo, index: number) => (
-                                                <div key={vuelo.id}>
-                                                    <section className='flex flex-row my-4 justify-between *:flex *:flex-col px-2'>
-                                                        <div className='*:text-left'>
-                                                            <Small>{vuelo.planVuelo.ciudadOrigen.ciudad}</Small>
-                                                            <Muted>
-                                                                Tiempo en almacen:{" "}
-                                                                {(() => {
-                                                                    if (index === 0) {
-                                                                        let tiempoRegistro = new Date(shipment?.fechaRecepcion);
-                                                                        let tiempoSalida = new Date(vuelo?.fechaSalida);
-                                                                        if (
-                                                                            isNaN(tiempoRegistro.getTime()) ||
-                                                                            isNaN(tiempoSalida.getTime())
-                                                                        )
-                                                                            return "Fecha no válida";
-
-                                                                        let diffTime = tiempoSalida.getTime() - tiempoRegistro.getTime();
-                                                                        let diffHours = Math.floor(diffTime / (1000 * 60 * 60));
-                                                                        let diffMinutes = Math.floor(
-                                                                            (diffTime % (1000 * 60 * 60)) / (1000 * 60)
-                                                                        );
-                                                                        return diffHours === 0
-                                                                            ? `${diffMinutes} minutos`
-                                                                            : `${diffHours} horas y ${diffMinutes} minutos`;
-                                                                    } else {
-                                                                        if (
-                                                                            selectedPackage?.planRutaActual?.vuelos[index - 1] === undefined
-                                                                        )
-                                                                            return "";
-                                                                        let tiempoLlegada = new Date(
-                                                                            selectedPackage?.planRutaActual?.vuelos[index - 1]?.fechaLlegada
-                                                                        );
-                                                                        let tiempoSalida = new Date(vuelo.fechaSalida);
-                                                                        if (isNaN(tiempoLlegada.getTime()) || isNaN(tiempoSalida.getTime()))
-                                                                            return "Fecha no válida";
-
-                                                                        let diffTime = tiempoSalida.getTime() - tiempoLlegada.getTime();
-                                                                        let diffHours = Math.floor(diffTime / (1000 * 60 * 60));
-                                                                        let diffMinutes = Math.floor(
-                                                                            (diffTime % (1000 * 60 * 60)) / (1000 * 60)
-                                                                        );
-                                                                        return diffHours === 0
-                                                                            ? `${diffMinutes} minutos`
-                                                                            : `${diffHours} horas y ${diffMinutes} minutos`;
-                                                                    }
-                                                                })()}
-                                                            </Muted>
-                                                        </div>
-                                                    </section>
-                                                    <section className='flex flex-row my-4 justify-between *:flex *:flex-col px-2'>
-                                                        <div className='*:text-left'>
-                                                            <Small>{vuelo.planVuelo.ciudadOrigen.ciudad}</Small>
-                                                            <Muted>{formatDateTimeLongShort(vuelo.fechaSalida)}</Muted>
-                                                        </div>
-                                                        <div className='*:text-right'>
-                                                            <Small>{vuelo.planVuelo.ciudadDestino.ciudad}</Small>
-                                                            <Muted>{formatDateTimeLongShort(vuelo.fechaLlegada)}</Muted>
-                                                        </div>
-                                                    </section>
-                                                </div>
-                                            ))}
+                                            <Stepper
+                                                index={activeStep}
+                                                orientation='vertical'
+                                                height='max-content'
+                                                gap={0}
+                                            >
+                                                {stepsData.map((step) => (
+                                                    <Step
+                                                        key={step.id}
+                                                        className='w-full pr-4'
+                                                    >
+                                                        <StepIndicator>
+                                                            <StepStatus
+                                                                complete={<StepIcon />}
+                                                                incomplete={<StepNumber />}
+                                                                active={<StepNumber />}
+                                                            />
+                                                        </StepIndicator>
+                                                        <Box className='w-full'>
+                                                            {step.type === "aeropuerto" ? (
+                                                                <>
+                                                                    <StepTitle>{step.ciudad}</StepTitle>
+                                                                    <StepDescription>
+                                                                        Tiempo en almacén: {step.tiempoEnAlmacen}
+                                                                    </StepDescription>
+                                                                </>
+                                                            ) : (
+                                                                <>
+                                                                    <div className='flex flex-row justify-between'>
+                                                                        <div className='flex flex-col text-left'>
+                                                                            <StepTitle>{`${step.origen}`}</StepTitle>
+                                                                            <StepDescription>{`${formatDateTimeLongShort(step.fechaSalida)}`}</StepDescription>
+                                                                        </div>
+                                                                        <div className='flex flex-col text-right'>
+                                                                            <StepTitle>{`${step.destino}`}</StepTitle>
+                                                                            <StepDescription>{`${formatDateTimeLongShort(step.fechaLlegada)}`}</StepDescription>
+                                                                        </div>
+                                                                    </div>
+                                                                </>
+                                                            )}
+                                                            <Box className='w-full h-4 bg-transparent' />
+                                                        </Box>
+                                                        <StepSeparator />
+                                                    </Step>
+                                                ))}
+                                            </Stepper>
                                         </div>
                                     </div>
                                 )}
