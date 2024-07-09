e85ce418526fa77e50d792678cbe9625baf4a53b
Jose <a20190202@pucp.edu.pe>
2024-07-09 18:01:51 -0500

    cambio param dia dia

--- backend/redex_back/src/main/java/pucp/e3c/redex_back/DataInitializer.java
+++ backend/redex_back/src/main/java/pucp/e3c/redex_back/DataInitializer.java
@@ -243,7 +243,7 @@ public class DataInitializer {
             algoritmo.loopPrincipalDiaADia(aeropuertosLoop, planVuelosLoop,
                     vueloService, planRutaService, paqueteService, planRutaXVueloService, aeropuertoService,
                     //60, 40);
-                    240, 60);
+                    240, 120);
         });
 
     }
