3ebf7424daa070eb7dc11bcd51cb001d4b04579a
Jose <a20190202@pucp.edu.pe>
2024-07-10 21:51:32 -0500

    custom clock

--- backend/redex_back/src/main/java/pucp/e3c/redex_back/config/CustomClockConfig.java
+++ backend/redex_back/src/main/java/pucp/e3c/redex_back/config/CustomClockConfig.java
@@ -5,15 +5,45 @@ import java.time.Instant;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @Configuration
 public class CustomClockConfig {
     
-    @Bean
+    /*@Bean
     public Clock customClock() {
         Clock baseClock = Clock.systemDefaultZone();
         Instant fixedInstant = Instant.parse("2024-07-22T10:45:00Z");
         return Clock.offset(baseClock, Duration.between(Instant.now(), fixedInstant));
+    }*/
+
+    @Bean
+    public ClockHolder clockHolder() {
+        return new ClockHolder();
+    }
+
+    @Bean
+    public Clock customClock(ClockHolder clockHolder) {
+        return clockHolder.getClock();
+    }
+
+    @Component
+    public static class ClockHolder {
+        private Clock clock;
+
+        public ClockHolder() {
+            resetClock();
+        }
+
+        public Clock getClock() {
+            return clock;
+        }
+
+        public void resetClock() {
+            Clock baseClock = Clock.systemDefaultZone();
+            Instant fixedInstant = Instant.parse("2024-07-22T10:45:00Z");
+            this.clock = Clock.offset(baseClock, Duration.between(Instant.now(), fixedInstant));
+        }
     }
 
 }
--- backend/redex_back/src/main/java/pucp/e3c/redex_back/controller/TimeController.java
+++ backend/redex_back/src/main/java/pucp/e3c/redex_back/controller/TimeController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class TimeController {
     @GetMapping("/now")
     public Date now() {
         return timeService.now();
+    }
+
+    @PostMapping("/reset")
+    public void resetClock() {
+        timeService.resetClock();
     }
 }
